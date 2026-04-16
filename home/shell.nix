f580e6a5a7e450bc093c268687e74b44c7092ffe
nick <11924961+nickwelsh@users.noreply.github.com>
2026-04-16 11:15:29 -0400

    feat: ✨ Use username from nix

--- home/shell.nix
+++ home/shell.nix
@@ -27,28 +27,28 @@
       # [[ -f "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh" ]] && builtin source "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh"
 
       # Herd injected NVM configuration
-      export NVM_DIR="/Users/nick/Library/Application Support/Herd/config/nvm"
+      export NVM_DIR="/Users/${username}/Library/Application Support/Herd/config/nvm"
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
       # Herd injected PHP binary.
       path+=("/Users/${username}/Library/Application Support/Herd/bin/")
 
       # Herd injected PHP 8.3 configuration.
-      export HERD_PHP_83_INI_SCAN_DIR="/Users/nick/Library/Application Support/Herd/config/php/83/"
+      export HERD_PHP_83_INI_SCAN_DIR="/Users/${username}/Library/Application Support/Herd/config/php/83/"
 
       # Herd injected PHP 8.4 configuration.
-      export HERD_PHP_84_INI_SCAN_DIR="/Users/nick/Library/Application Support/Herd/config/php/84/"
+      export HERD_PHP_84_INI_SCAN_DIR="/Users/${username}/Library/Application Support/Herd/config/php/84/"
 
       # Herd injected PHP 8.5 configuration.
-      export HERD_PHP_85_INI_SCAN_DIR="/Users/nick/Library/Application Support/Herd/config/php/85/"
+      export HERD_PHP_85_INI_SCAN_DIR="/Users/${username}/Library/Application Support/Herd/config/php/85/"
 
       # ---------- #
       # -- PATH -- #
       # ---------- #
 
-      path+=($PATH, "/Users/nick/.bun/bin")
-      path+=($PATH, "/Users/nick/.local/bin")
-      path+=($path, "/Users/nick/.vite-plus/env")
+      path+=($PATH, "/Users/${username}/.bun/bin")
+      path+=($PATH, "/Users/${username}/.local/bin")
+      path+=($path, "/Users/${username}/.vite-plus/env")
     '';
   };
 
